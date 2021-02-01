d882e792c582a4fdb686923b71986fd999545799
Oleg Pilyak <72813391+KnightSlayerTsorig@users.noreply.github.com>
2021-02-02 00:39:34 +0200

    Update settings.py

--- konoha_today/settings.py
+++ konoha_today/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'z2tp%a^hml@13(uw29*%^jt%1ofrpjei7^fg1#aehkvc2dlti^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
